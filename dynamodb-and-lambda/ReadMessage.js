895a4d66869d5cb465f9ce6eb8b995f74ec35301
Joanne Skiles <chaotictoejam@users.noreply.github.com>
2020-04-21 11:04:58 -0400

    Add Comments to ReadMessage.js

--- dynamodb-and-lambda/ReadMessage.js
+++ dynamodb-and-lambda/ReadMessage.js
@@ -1,12 +1,18 @@
-const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+// Loads in the AWS SDK
+const AWS = require('aws-sdk'); 
+
+// Creates the document client specifing the region 
+// The tutorial's table is 'in us-east-1'
+const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'}); 
 
 exports.handler = async (event, context, callback) => {
+    // Handle promise fulfilled/rejected states
     await readMessage().then(data => {
         data.Items.forEach(function(item) {
             console.log(item.message)
         });
         callback(null, {
+            // If success return 200, and items
             statusCode: 200,
             body: data.Items,
             headers: {
@@ -14,10 +20,14 @@ exports.handler = async (event, context, callback) => {
             },
         })
     }).catch((err) => {
+        // If an error occurs write to the console
         console.error(err);
     })
 };
 
+// Function readMessage
+// Reads 10 messages from the DynamoDb table Message
+// Returns promise
 function readMessage() {
     const params = {
         TableName: 'Message',
