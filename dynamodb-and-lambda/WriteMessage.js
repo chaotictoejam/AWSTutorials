91c45f9830459d2283713385506857588ac3dfcc
Joanne Skiles <chaotictoejam@users.noreply.github.com>
2020-04-21 10:10:36 -0400

    Added Comments to WriteMessage.js

--- dynamodb-and-lambda/WriteMessage.js
+++ dynamodb-and-lambda/WriteMessage.js
@@ -1,8 +1,15 @@
+// Loads in the AWS SDK
 const AWS = require('aws-sdk');
+
+// Creates the document client specifing the region 
+// The tutorial's table is 'in us-east-1'
 const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 
 exports.handler = async (event, context, callback) => {
+    // Captures the requestId from the context message
     const requestId = context.awsRequestId;
+
+    // Handle promise fulfilled/rejected states
     await createMessage(requestId).then(() => {
         callback(null, {
             statusCode: 201,
@@ -16,6 +23,8 @@ exports.handler = async (event, context, callback) => {
     })
 };
 
+// Function createMessage
+// Writes message to DynamoDb table Message 
 function createMessage(requestId) {
     const params = {
         TableName: 'Message',
